afc9249aef8720063c12fbadbebed24bf4b10983
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 08:53:54 +0530

    if Statement

---
+++ 1.JavaScript if Statement.js
@@ -0,0 +1,11 @@
+// check if the number is positive
+
+const number = 2;
+
+// check if number is greater than 0
+if (number > 0) {
+ // the body of the if statement
+  console.log("The number is positive");//The number is positive
+}
+
+console.log("The if statement is easy");//The if statement is easy
