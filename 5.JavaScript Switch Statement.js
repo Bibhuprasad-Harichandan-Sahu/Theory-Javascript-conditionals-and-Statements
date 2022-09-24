6e98621878e65ca241cafa1b4f54d2fe6c18c62e
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 09:56:06 +0530

    Switch statement

--- 5.JavaScript Switch Statement.js
+++ 5.JavaScript Switch Statement.js
@@ -15,3 +15,25 @@ switch (a) {
 }
 console.log(`The value is ${a}`);
 //output-The value is two
+
+
+// program using switch statement
+let z = 1;
+
+switch (z) {
+    case "1":
+        z = 1;
+        break;
+    case 1:
+        z = 'one';
+        break;
+    case 2:
+        z = 'two';
+        break;
+
+    default:
+        z = 'not found';
+        break;
+}
+console.log(`The value is ${z}`);
+//output-The value is one
