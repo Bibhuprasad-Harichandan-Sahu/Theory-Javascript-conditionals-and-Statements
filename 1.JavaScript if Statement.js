a44470baf8c43aced89599728dc7173435859dbf
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 08:59:07 +0530

    If Statement

--- 1.JavaScript if Statement.js
+++ 1.JavaScript if Statement.js
@@ -9,3 +9,9 @@ if (number > 0) {
 }
 
 console.log("The if statement is easy");//The if statement is easy
+
+
+let num = 10;
+if(num <20){
+    console.log("the statement is true");the statement is true
+}
